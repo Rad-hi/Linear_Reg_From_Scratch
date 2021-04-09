4bababe4a40869b0719ccab5bafcdfd44cb078dd
Rad-hi <radhisghaier33@gmail.com>
2021-04-09 02:02:22 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,35 @@
+# 2D Linear Regression from scratch with Python
+
+---
+
+# Overview
+
+This is a bibliographic dive into the basics of machine learning (ML) and how it actually works under the hoods of all available frameworks (sklearn, tf, pytorch, ...)
+
+This code is basically not mine, I copy pasted and got inspired from a couple of sources, which I'll be listing in the references section of this README.
+
+It's a simple class that contains functions for random linear data genration, external data importing, and linear regression with two methods:
+
+- Gradient descent (iterative method).
+
+- Best slope and intercept (instant method).
+
+This code is for 2D linear regression, these methods get a bit complicated when implemented for dimensions higher than 2 (basically once we stop looking for lines and we switch to hyper planes ("lines" in multi-dimensions, example: a 2D-plane is a 3D-space hyper-plane, ...).
+
+I might be implementing a Pseudo-inverse method for multi-dimensional linear regression (instant method) in the future.
+
+---
+
+# References: 
+
+- [Article 1: https://www.askpython.com/python/examples/linear-regression-from-scratch]( https://www.askpython.com/python/examples/linear-regression-from-scratch)
+
+- [Article 2: https://developer.ibm.com/technologies/artificial-intelligence/articles/linear-regression-from-scratch/]( https://developer.ibm.com/technologies/artificial-intelligence/articles/linear-regression-from-scratch/)
+
+- [Youtube Video: https://www.youtube.com/watch?v=JcI5Vnw0b2c&t=279s](https://www.youtube.com/watch?v=JcI5Vnw0b2c&t=279s)
+
+---
+
+# Author info
+
+[Radhi SGHAIER](https://www.linkedin.com/in/radhi-sghaier/)
---
+++ scripts/Simple_2D_Linear_Regression_Example.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from TwoD_LinearRegression import TwoD_LinearRegression
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+from time import time
+
+start = time()
+
+style.use('seaborn-whitegrid')
+
+# Creating the class object
+regressor = TwoD_LinearRegression()
+
+x, y = regressor.create_data(80, 30, 4, correlation = "negative")
+
+#############################################
+###    Load data from an external file    ###
+#############################################
+
+"""
+# Don't forget to import pandas
+#import pandas as pd
+
+# Loading the data from an external file
+df = pd.read_csv('DATA_FILE_NAME.csv')
+
+# Prepare the data
+x = np.array(df.iloc[:,0])
+y = np.array(df.iloc[:,1])
+
+# Load the data into the class instance
+regressor.assign_data(x, y)
+"""
+
+#############################
+###    Train the model    ###
+#############################
+
+# Training the model: 
+# --> 1st mehod is by using gradient descent to optimize the weights
+# --> 2nd method is by calculating the slope and intercept
+# Uncomment the desired method
+
+#regressor.fit_grad(1000 , 0.0001) # epochs-1000 , learning_rate - 0.0001
+regressor.best_fit_slope_intercept()
+
+###########################
+###    Use the model    ###
+###########################
+
+# Prediciting the values
+y_pred = regressor.predict(x)
+
+# Evaluate the model by calculating its coefficient of determination
+r2 = regressor.coef_det(y_pred)
+
+#
+print("The coefficient of determination is equal to: {}".format(r2))
+print("The code ran in: {} seconds".format(time()-start))
+ 
+# Plotting the results
+plt.scatter(x,y)
+plt.plot(x , y_pred, color = "black")
+plt.xlabel('x' , size = 10)
+plt.ylabel('y', size = 10)
+plt.show()
