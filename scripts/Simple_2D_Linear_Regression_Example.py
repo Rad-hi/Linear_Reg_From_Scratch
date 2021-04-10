c352874181a8d1ebc96713b581b94fd294595afc
Rad-hi <radhisghaier33@gmail.com>
2021-04-10 12:56:34 +0100

    Added a static-randomly-generated dataset for consisting testing

--- scripts/Simple_2D_Linear_Regression_Example.py
+++ scripts/Simple_2D_Linear_Regression_Example.py
@@ -2,6 +2,7 @@
 
 from TwoD_LinearRegression import TwoD_LinearRegression
 
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 from time import time
@@ -10,10 +11,22 @@ start = time()
 
 style.use('seaborn-whitegrid')
 
+Test_X = [x for x in range(80) ]
+
+Test_Y = [ -15.0,    7.0,  -18.0,   -4.0,  -29.0,    4.0,    2.0,  -16.0,  -32.0,  -35.0,  -59.0,  -59.0
+		  ,-21.0,  -30.0,  -66.0,  -33.0,  -66.0,  -70.0,  -54.0,  -50.0,  -91.0, -75.0,  -98.0,  -77.0
+		  ,-92.0,  -85.0, -120.0, -137.0,  -98.0,  -91.0, -149.0, -128.0, -100.0, -135.0, -156.0, -161.0
+		  ,-129.0, -146.0, -145.0, -147.0, -132.0, -163.0, -172.0, -201.0, -194.0, -163.0, -205.0, -198.0
+		  ,-179.0, -213.0, -191.0, -177.0, -206.0, -198.0, -201.0, -237.0, -221.0, -213.0, -221.0, -257.0
+		  ,-232.0, -225.0, -251.0, -242.0, -254.0, -275.0, -267.0, -267.0, -300.0, -297.0, -305.0, -297.0
+		  ,-294.0, -303.0, -268.0, -311.0, -311.0, -293.0, -338.0, -333.0]
+
 # Creating the class object
 regressor = TwoD_LinearRegression()
 
-x, y = regressor.create_data(80, 30, 4, correlation = "negative")
+#x, y = regressor.create_data(80, 30, 4, correlation = "negative")
+x, y = np.array(Test_X, dtype = np.float64),np.array(Test_Y, dtype = np.float64)
+regressor.assign_data(x, y)
 
 #############################################
 ###    Load data from an external file    ###
@@ -43,8 +56,8 @@ regressor.assign_data(x, y)
 # --> 2nd method is by calculating the slope and intercept
 # Uncomment the desired method
 
-regressor.fit_grad(100, 0.0001) # epochs-1000 , learning_rate - 0.0001
-#regressor.best_fit_slope_intercept()
+#regressor.fit_grad(100, 0.0001) # epochs-1000 , learning_rate - 0.0001
+regressor.best_fit_slope_intercept()
 
 ###########################
 ###    Use the model    ###
